7af2c008eee88720111315b709ee5087d21e151d
Taras Navakhatska <taras.navakhatska@leverx.com>
2022-04-21 12:01:07 +0300

    New Data values, Problem with timestatus classes

--- main.js
+++ main.js
@@ -20,7 +20,16 @@ const renderList = () => {
     p3.classList.add("orders-info");
 
     const p4 = document.createElement("p");
-    /*p4.classList.add("orders-number");????????????????*/
+    /*p4.classList.add(getClassByStatus);????????????????*/
+
+    const p5 = document.createElement("p");
+    p5.classList.add("shipped-time-urgent");
+
+    const p6 = document.createElement("p");
+    p6.classList.add("shipped-time-in");
+
+    const p7 = document.createElement("p");
+    p7.classList.add("shipped-time-late");
 
     p1.innerText = "Order " + element.OrderID;
     p2.innerText = parseDate(element.OrderDate);
@@ -28,14 +37,19 @@ const renderList = () => {
     const t1 = document.createTextNode(element.ShipName);
     const br = document.createElement("br");
     const t2 = document.createTextNode(
-      "Shipped: " + parseDate(element.ShippedDate)
+      "Shipped: " + parseDate1(element.ShippedDate)
     );
 
     p3.append(t1, br, t2);
-    p4.innerText = timeStatus(element.ShippedDate, element.RequiredDate);
+
+    const timeStatus = getTimeStatus(element.ShippedDate, element.RequiredDate);
+    p4.innerText = timeStatus;
+    /*p5.innerText = timeStatus;
+    p6.innerText = timeStatus;
+    p7.innerText = timeStatus;*/
 
     divContainer1.append(p1, p2);
-    divContainer2.append(p3, p4);
+    divContainer2.append(p3, p4 /*p5, p6, p7*/);
 
     div.append(divContainer1, divContainer2);
 
@@ -44,27 +58,32 @@ const renderList = () => {
 };
 renderList();
 
-function parseDate(OrderDate) {
+function parseDate(elem) {
   /*parse numbers*/
-  const numEl = parseInt(OrderDate.match(/\d+/));
+  const numEl = parseInt(elem.match(/\d+/));
 
-  /*convert to date*/ /* only month + 1??? */
-  const d = new Date(numEl);
-  const t =
-    d.getMonth() +
-    1 +
-    "/" +
-    d.getDay() +
-    "/" +
-    d
-      .getFullYear()
-      .toString()
-      .substr(-2); /*.toString().substr(-2) -------last 2 numbers*/
-  return t;
+  /*convert to date*/
+  const d = new Date(numEl).toLocaleString("en", {
+    year: "2-digit",
+    day: "numeric",
+    month: "numeric",
+  });
+  return d;
 }
 
-/*function timestatus()*/
-function timeStatus(ShippedDate, RequiredDate) {
+function parseDate1(elem) {
+  const numEl = parseInt(elem.match(/\d+/));
+
+  const d = new Date(numEl).toLocaleString("en", {
+    year: "numeric",
+    day: "numeric",
+    month: "long",
+  });
+  return d;
+}
+
+/*function getTimeStatus()*/
+function getTimeStatus(ShippedDate, RequiredDate) {
   if (RequiredDate > ShippedDate) {
     return "Urgent";
   } else if (RequiredDate === ShippedDate) {
@@ -73,3 +92,24 @@ function timeStatus(ShippedDate, RequiredDate) {
     return "Too late";
   }
 }
+
+/*function getClassByStatus()
+function getClassByStatus(timeStatus) {
+  if (timeStatus === "Urgent") {
+    return p5;
+  } else if (timeStatus === "In time") {
+    return p6;
+  } else {
+    return p7;
+  }
+}
+
+/*function getTimeStatus(ShippedDate, RequiredDate) {
+  if (RequiredDate > ShippedDate) {
+    return p5;
+  } else if (RequiredDate === ShippedDate) {
+    return p6;
+  } else {
+    return p7;
+  }
+}*/
