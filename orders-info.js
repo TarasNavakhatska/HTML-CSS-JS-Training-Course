6af6d4a1626afed1faed23818f103f3d45e1d187
Taras Navakhatska <taras.navakhatska@leverx.com>
2022-05-24 11:35:21 +0300

    Shipping address

--- orders-info.js
+++ orders-info.js
@@ -1,23 +1,46 @@
-/*const orderInfo = () => {
-  const info = document.getElementById("render_click");
-  listOrder.d.results.forEach((element) => {
-    const divOrder = document.createElement("div");
-    divOrder.classList.add("description-block");
-
-    const divHeader = document.createElement("div");
-    divHeader.classList.add("right-header");
-    divHeader.innerText = "HHHH" + element.ShipVia;
-
-    divOrder.appendChild(divHeader);
-
-    info.appendChild(divOrder);
-  });
-};
-
-orderInfo();*/
-//tableInfo();
 const tableInfo = (order) => {
-  //console.log(order.Order_Details.results);
+  /* get Shipping address*/
+  const titleProduct = document.getElementById("titleProduct");
+  const streetProduct = document.getElementById("streetProduct");
+  const zipProduct = document.getElementById("zipProduct");
+  const regionProduct = document.getElementById("regionProduct");
+  const countryProduct = document.getElementById("countryProduct");
+
+  titleProduct.innerHTML = "";
+  streetProduct.innerHTML = "";
+  zipProduct.innerHTML = "";
+  regionProduct.innerHTML = "";
+  countryProduct.innerHTML = "";
+
+  listOrder.d.results.forEach((element) => {
+    if (element.OrderID === order.OrderID) {
+      const title = document.createElement("span");
+      title.classList.add("name-value");
+      title.innerText = element.ShipName;
+      titleProduct.appendChild(title);
+
+      const street = document.createElement("span");
+      street.classList.add("name-value");
+      street.innerText = element.ShipAddress;
+      streetProduct.appendChild(street);
+
+      const zip = document.createElement("span");
+      zip.classList.add("name-value");
+      zip.innerText = element.ShipPostalCode;
+      zipProduct.appendChild(zip);
+
+      const region = document.createElement("span");
+      region.classList.add("name-value");
+      region.innerText = element.ShipRegion;
+      regionProduct.appendChild(region);
+
+      const country = document.createElement("span");
+      country.classList.add("name-value");
+      country.innerText = element.ShipCountry;
+      countryProduct.appendChild(country);
+    }
+  });
+  /*get table data*/
   const tbody = document.getElementById("tbody");
   tbody.innerHTML = "";
 
@@ -28,13 +51,14 @@ const tableInfo = (order) => {
     td1.classList.add("item-name");
     const nodeB = document.createElement("b");
     nodeB.innerText = element.Product.ProductName;
-    console.log(element.Product);
+    //console.log(element.Product);
+
     td1.appendChild(nodeB);
     const td2 = document.createElement("td");
     td2.classList.add("item-name");
     td2.innerText = element.Product.UnitPrice + " EUR";
-    //console.log(element);
-
+    console.log(element);
+    console.log(element.Product.ProductName);
     const td3 = document.createElement("td");
     td3.classList.add("text-table-right");
     td3.innerText = element.Quantity;
@@ -48,8 +72,4 @@ const tableInfo = (order) => {
 
     tbody.appendChild(tr1);
   });
-
-  //order.d.results.forEach((element) => {
-  //console.log(element);
-  //});
 };
