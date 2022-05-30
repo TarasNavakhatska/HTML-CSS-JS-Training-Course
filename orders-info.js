13a32a9d3d789b48269c982c2532e350b21d51e9
Taras Navakhatska <taras.navakhatska@leverx.com>
2022-05-30 15:42:38 +0300

    Header right block

--- orders-info.js
+++ orders-info.js
@@ -1,19 +1,56 @@
 const tableInfo = (order) => {
   /* get Shipping address*/
+  const ordersNumber = document.getElementById("ordersNumber");
+  const customerName = document.getElementById("customerName");
+  const orderedTime = document.getElementById("orderedTime");
+  const shippedTime = document.getElementById("shippedTime");
+  const totalPrice = document.getElementById("totalPrice");
   const titleProduct = document.getElementById("titleProduct");
   const streetProduct = document.getElementById("streetProduct");
-  const zipProduct = document.getElementById("zipProduct");
+  const zipCityProduct = document.getElementById("zip-cityProduct");
   const regionProduct = document.getElementById("regionProduct");
   const countryProduct = document.getElementById("countryProduct");
+  /*const lineItems = document.getElementById("lineItems");*/
 
+  ordersNumber.innerHTML = "";
+  customerName.innerHTML = "";
+  orderedTime.innerHTML = "";
+  shippedTime.innerHTML = "";
+  //totalPrice.innerHTML = "";
   titleProduct.innerHTML = "";
   streetProduct.innerHTML = "";
-  zipProduct.innerHTML = "";
+  zipCityProduct.innerHTML = "";
   regionProduct.innerHTML = "";
   countryProduct.innerHTML = "";
+  /*lineItems.innerHTML = "";*/
 
-  listOrder.d.results.forEach((element) => {
+  listOrder.d.results.find((element) => {
     if (element.OrderID === order.OrderID) {
+      const orders = document.createElement("span");
+      orders.classList.add("count-orders");
+      orders.innerText = "Order " + element.OrderID;
+      ordersNumber.appendChild(orders);
+
+      const customer = document.createElement("span");
+      customer.innerText = element.ShipName;
+      customerName.appendChild(customer);
+
+      const timeOrder = document.createElement("span");
+      timeOrder.innerText = parseDate1(element.OrderDate);
+      orderedTime.appendChild(timeOrder);
+
+      const timeShipped = document.createElement("span");
+      timeShipped.innerText = parseDate1(element.ShippedDate);
+      shippedTime.appendChild(timeShipped);
+
+      /*const price = document.createElement("span");
+       price.innerText =
+        element.Order_Details.results.Quantity *
+          element.Order_Details.results.UnitPrice +
+        "EUR"; 
+      console.log(price);
+      totalPrice.appendChild(price);*/
+
       const title = document.createElement("span");
       title.classList.add("name-value");
       title.innerText = element.ShipName;
@@ -24,10 +61,10 @@ const tableInfo = (order) => {
       street.innerText = element.ShipAddress;
       streetProduct.appendChild(street);
 
-      const zip = document.createElement("span");
-      zip.classList.add("name-value");
-      zip.innerText = element.ShipPostalCode;
-      zipProduct.appendChild(zip);
+      const zipCity = document.createElement("span");
+      zipCity.classList.add("name-value");
+      zipCity.innerText = element.ShipPostalCode + "  " + element.ShipCity;
+      zipCityProduct.appendChild(zipCity);
 
       const region = document.createElement("span");
       region.classList.add("name-value");
@@ -38,13 +75,20 @@ const tableInfo = (order) => {
       country.classList.add("name-value");
       country.innerText = element.ShipCountry;
       countryProduct.appendChild(country);
+
+      /* const line = document.createElement("div");
+      line.classList.add("caption-order-details");
+      line.innerText = ;
+      lineItems.appendChild(line);*/
     }
   });
+
   /*get table data*/
   const tbody = document.getElementById("tbody");
   tbody.innerHTML = "";
+  totalPrice.innerHTML = "";
 
-  order.Order_Details.results.forEach((element) => {
+  order.Order_Details.results.find((element) => {
     const tr1 = document.createElement("tr");
     tr1.classList.add("text-table");
     const td1 = document.createElement("td");
@@ -56,16 +100,16 @@ const tableInfo = (order) => {
     td1.appendChild(nodeB);
     const td2 = document.createElement("td");
     td2.classList.add("item-name");
-    td2.innerText = element.Product.UnitPrice + " EUR";
+    td2.innerText = element.UnitPrice + " EUR";
     console.log(element);
     console.log(element.Product.ProductName);
     const td3 = document.createElement("td");
     td3.classList.add("text-table-right");
     td3.innerText = element.Quantity;
-    //console.log(element.Quantity);
+    console.log(element.Quantity);
     const td4 = document.createElement("td");
     td4.classList.add("text-table-right");
-    const total = element.Quantity * element.Product.UnitPrice;
+    const total = element.Quantity * element.UnitPrice;
     td4.innerText = total + " EUR";
 
     tr1.append(td1, td2, td3, td4);
